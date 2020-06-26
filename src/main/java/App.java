3abfa43c5fdd7807a06abc7cc452e372adf9947d
edithntinyari <emothegi@gmail.com>
2020-06-26 14:22:27 +0300

    the first testing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,17 @@
+import java.io.Console;
+
 public class App {
+    public static void main(String[] args) {
+        Console myConsole = System.console();
+        System.out.println("Input string:");
+        String wordResponse = myConsole.readLine();
+        System.out.println("Key:");
+        String keyResponse = myConsole.readLine();
+        int intKeyResponse = Integer.parseInt(keyResponse);
+        CaesarCipher caesarCipher = new CaesarCipher(wordResponse, intKeyResponse);
+        String encryptionResult = caesarCipher.indexOfWord();
+        System.out.println("Encrypted String:" + encryptionResult + ".");
+        String finalOutput = caesarCipher.decryptedWord();
+        System.out.println("Decrypted string:" + finalOutput + ".");
+    }
 }
