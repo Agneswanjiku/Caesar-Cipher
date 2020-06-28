95927537a75a99e532eede034ac8895f5ebd04f9
edithntinyari <emothegi@gmail.com>
2020-06-28 11:32:36 +0300

    added encryptTest.java file

---
+++ src/test/java/EncryptTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class EncryptTest {
+
+    @Test
+    public void getmInputString() {
+    }
+
+    @Test
+    public void getmShift() {
+    }
+
+    @Test
+    public void encrypt() {
+    }
+}
