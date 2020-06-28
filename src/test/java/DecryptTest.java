8b7fa691a3fc0e192350e24134084d010e80c5a7
edithntinyari <emothegi@gmail.com>
2020-06-28 11:21:23 +0300

    added DescryptTest.java file

---
+++ src/test/java/DecryptTest.java
@@ -0,0 +1,27 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class DecryptTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getmInputString() {
+    }
+
+    @Test
+    public void getmShift() {
+    }
+
+    @Test
+    public void decrypt() {
+    }
+}
